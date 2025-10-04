579ea96546c13b7342abccda310c2b9b655e1a0a
yueqiangwu <wu147@illinois.edu>
2025-10-03 19:31:22 -0500

    2025-10-3

--- gpu/myconv.py
+++ gpu/myconv.py
@@ -3,8 +3,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.profiler import profile, record_function, ProfilerActivity
 
+
 class ConvModel(nn.Module):
-    def __init__(self, H, W, in_channels=3, out_channels=8, kernel_size=3, stride=1, padding=1):
+    def __init__(
+        self, H, W, in_channels=3, out_channels=8, kernel_size=3, stride=1, padding=1
+    ):
         super().__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -16,19 +19,22 @@ class ConvModel(nn.Module):
         self.H = H
         self.W = W
 
-        # TO DO: Define static shapes here. 
+        # TO DO: Define static shapes here.
+        self.TILE_SIZE = 1024
 
         # Precompute output size
         # self.out_h = ...
         # self.out_w = ...
+        self.out_h = (H + 2 * padding - kernel_size) // stride + 1
+        self.out_w = (W + 2 * padding - kernel_size) // stride + 1
 
-        self.weight = nn.Parameter(torch.randn(out_channels, in_channels, kernel_size, kernel_size))
+        self.weight = nn.Parameter(
+            torch.randn(out_channels, in_channels, kernel_size, kernel_size)
+        )
         self.bias = nn.Parameter(torch.zeros(out_channels))
 
-        
-
     def im2col_manual(self, x):
-        N = x.shape[0]        # batch size can remain dynamic
+        N = x.shape[0]  # batch size can remain dynamic
         C = self.in_channels
         KH = KW = self.kernel_size
         S = self.stride
@@ -39,31 +45,59 @@ class ConvModel(nn.Module):
         # Pad input
         x_pad = F.pad(x, (P, P, P, P))
 
-        # TO DO: Convert input (x) into shape (N, out_h*out_w, C*KH*KW). 
+        # TO DO: Convert input (x) into shape (N, out_h*out_w, C*KH*KW).
         # Refer to Lecture 3 for implementing this operation.
-        
+
         # patches = ...
+        patches = []
+        for i in range(0, KH):
+            i_end = i + S * out_h
+            for j in range(0, KW):
+                j_end = j + S * out_w
+                t = x_pad[:, :, i:i_end:S, j:j_end:S]
+                patches.append(t)
+
+        patches = torch.stack(patches, dim=0)
+        patches = patches.permute(1, 3, 4, 2, 0)
+        patches = patches.reshape(N, out_h * out_w, C * KH * KW)
+
         # return patches
+        return patches
 
     def conv2d_manual(self, x):
         N = x.shape[0]
         C_out = self.out_channels
         KH = KW = self.kernel_size
+        out_h = self.out_h
+        out_w = self.out_w
+        T = self.TILE_SIZE
 
         # TO DO: 1) convert input (x) into shape (N, out_h*out_w, C*KH*KW).
-        # cols = self.im2col_manual(x)          
+        # cols = self.im2col_manual(x)
+        cols = self.im2col_manual(x)
 
         # TO DO: 2) flatten self.weight into shape (C_out, C*KH*KW).
+        w_flat_t = self.weight.view(C_out, -1).t()
 
         # TO DO: 3) perform tiled matmul after required reshaping is done.
+        out_s = out_h * out_w
+        out = torch.empty((N, out_s, C_out), device=x.device)
+
+        for start in range(0, out_s, T):
+            end = min(out_s, start + T)
+            tile_cols = cols[:, start:end, :]
+            tile_out = torch.matmul(tile_cols, w_flat_t)
+            out[:, start:end, :] = tile_out
 
         # TO DO: 4) Add bias.
+        out = out + self.bias.view(1, 1, C_out)
 
         # TO DO: 5) reshape output into shape (N, C_out, out_h, out_w).
+        out = out.permute(0, 2, 1)
+        out = out.reshape(N, C_out, out_h, out_w)
 
-
-
-        #return out
+        # return out
+        return out
 
     def forward(self, x):
         return self.conv2d_manual(x)
@@ -71,12 +105,24 @@ class ConvModel(nn.Module):
 
 if __name__ == "__main__":
     torch.manual_seed(0)
-    N, C, H, W = 2, 4, 22, 22
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    N, C, H, W = 2, 4, 220, 220
     x = torch.randn(N, C, H, W)
-    out_channels=8
-    kernel_size=7
+    out_channels = 8
+    kernel_size = 7
     model = ConvModel(H, W, C, out_channels, kernel_size, stride=1, padding=1)
-    out = model(x)
+
+    # out = model(x)
+
+    with profile(
+        activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
+        on_trace_ready=torch.profiler.tensorboard_trace_handler("./log"),
+        record_shapes=True,
+        with_stack=True,
+    ) as prof:
+        with record_function("myconv_forward"):
+            out = model(x)
 
     # Test your solution
     conv_ref = F.conv2d(x, model.weight, model.bias, stride=1, padding=1)
--- gpu/myconv_jax.py
+++ gpu/myconv_jax.py
@@ -10,53 +10,90 @@ import jax.profiler
 # Create a log directory
 logdir = "./jax_trace"
 
+
 def im2col_manual_jax(x, KH, KW, S, P, out_h, out_w):
-    ''' 
-        Reimplement the same function (im2col_manual) in myconv.py "for JAX". 
-        Hint: Instead of torch tensors, use of jnp arrays is required to leverage JIT compilation and GPU execution in JAX
-    '''
+    """
+    Reimplement the same function (im2col_manual) in myconv.py "for JAX".
+    Hint: Instead of torch tensors, use of jnp arrays is required to leverage JIT compilation and GPU execution in JAX
+    """
     # x: (N, C, H, W)
     N, C, H, W = x.shape
 
     # Pad input
-    x_pad = jnp.pad(x, ((0,0),(0,0),(P,P),(P,P)))
+    x_pad = jnp.pad(x, ((0, 0), (0, 0), (P, P), (P, P)))
 
-    # TO DO: Convert input (x) into shape (N, out_h*out_w, C*KH*KW). 
+    # TO DO: Convert input (x) into shape (N, out_h*out_w, C*KH*KW).
     # Refer to Lecture 3 for implementing this operation.
-    
+
     # patches = ...
+    patches = []
+    for i in range(0, KH):
+        i_end = i + S * out_h
+        for j in range(0, KW):
+            j_end = j + S * out_w
+            t = x_pad[:, :, i:i_end:S, j:j_end:S]
+            patches.append(t)
+
+    patches = jnp.stack(patches, axis=0)
+    patches = jnp.transpose(patches, (1, 3, 4, 2, 0))
+    patches = jnp.reshape(patches, (N, out_h * out_w, C * KH * KW))
+
     # return patches
+    return patches
+
 
 def conv2d_manual_jax(x, weight, bias, stride=1, padding=1):
-    '''
-        Reimplement the same function (conv2d_manual) in myconv.py "for JAX". 
-        Hint: Instead of torch tensors, use of jnp arrays is required to leverage JIT compilation and GPU execution in JAX
-        Hint: Unlike PyTorch, JAX arrays are immutable, so you cannot do indexing like out[i:j, :] = ... inside a JIT. You may use .at[].set() instead.
-    '''
+    """
+    Reimplement the same function (conv2d_manual) in myconv.py "for JAX".
+    Hint: Instead of torch tensors, use of jnp arrays is required to leverage JIT compilation and GPU execution in JAX
+    Hint: Unlike PyTorch, JAX arrays are immutable, so you cannot do indexing like out[i:j, :] = ... inside a JIT. You may use .at[].set() instead.
+    """
     N, C, H, W = x.shape
     C_out, _, KH, KW = weight.shape
 
     # define your helper variables here
     # out_h = ...
     # out_w = ...
-    
+    out_h = (H + 2 * padding - KH) // stride + 1
+    out_w = (W + 2 * padding - KW) // stride + 1
+
     # TO DO: 1) convert input (x) into shape (N, out_h*out_w, C*KH*KW).
     # cols = im2col_manual_jax(x, KH, KW, stride, padding, out_h, out_w)
+    cols = im2col_manual_jax(x, KH, KW, stride, padding, out_h, out_w)
 
     # TO DO: 2) flatten self.weight into shape (C_out, C*KH*KW).
+    flat_w_t = jnp.reshape(weight, (C_out, -1)).T
 
     # TO DO: 3) perform tiled matmul after required reshaping is done.
+    TILE_SIZE = 1024
+    out_s = out_h * out_w
+    out = []
+
+    for start in range(0, out_s, TILE_SIZE):
+        end = min(out_s, start + TILE_SIZE)
+        tile_cols = cols[:, start:end, :]
+        tile_out = jnp.matmul(tile_cols, flat_w_t)
+        out.append(tile_out)
+
+    out = jnp.concatenate(out, axis=1)
 
     # TO DO: 4) Add bias.
+    out = out + jnp.reshape(bias, (1, 1, C_out))
 
     # TO DO: 5) reshape output into shape (N, C_out, out_h, out_w).
+    out = jnp.transpose(out, (0, 2, 1))
+    out = jnp.reshape(out, (N, C_out, out_h, out_w))
 
-    #return out
+    # return out
+    return out
+
 
 if __name__ == "__main__":
     # Instantiate PyTorch model
     H, W = 33, 33
-    model = ConvModel(H, W, in_channels=3, out_channels=8, kernel_size=5, stride=1, padding=1)
+    model = ConvModel(
+        H, W, in_channels=3, out_channels=8, kernel_size=5, stride=1, padding=1
+    )
     model.eval()
 
     # Example input
@@ -64,8 +101,10 @@ if __name__ == "__main__":
 
     # Export weights and biases
     params = {
-        "weight": model.weight.detach().cpu().numpy(),  # shape (out_channels, in_channels, KH, KW)
-        "bias": model.bias.detach().cpu().numpy()       # shape (out_channels,)
+        "weight": model.weight.detach()
+        .cpu()
+        .numpy(),  # shape (out_channels, in_channels, KH, KW)
+        "bias": model.bias.detach().cpu().numpy(),  # shape (out_channels,)
     }
 
     # Convert model input, weights and bias into jax arrays
@@ -77,7 +116,9 @@ if __name__ == "__main__":
     conv2d_manual_jax_jit = jit(conv2d_manual_jax)
 
     # call your JAX function
-    out_jax = conv2d_manual_jax_jit(x_jax, weight_jax, bias_jax)
+    out_jax = torch.from_numpy(
+        np.array(conv2d_manual_jax_jit(x_jax, weight_jax, bias_jax))
+    )
 
     # Test your solution
     conv_ref = F.conv2d(x_torch, model.weight, model.bias, stride=1, padding=1)
